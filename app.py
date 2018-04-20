0f67ebbb51e02e7eb3d8b05cf0dc59513ec939e3
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-20 15:09:38 +0530

    added logout code

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return "Hello Boss!!"
+        return "Hello Boss!! <a href='/logout'>Logout</a>"
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -42,7 +42,10 @@ def do_admin_login():
     else:
         flash("wrong password!!")
     return home()
-
+@app.route("/logout")
+def logout():
+    session['logged_in']=False
+    return home()
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
